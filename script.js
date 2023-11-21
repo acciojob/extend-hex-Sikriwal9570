251502aa53db4cedebe6a920bcf19bafebd03113
Sikriwal9570 <128585704+Sikriwal9570@users.noreply.github.com>
2023-11-21 11:04:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,19 @@
-const extendHex = (shortHex) => {
-  // write your code here
-};
+function extendHex(shortHex) {
+  // Remove the # if present
+  shortHex = shortHex.replace(/^#/, '');
 
-// Do not change the code below.
-const shortHex = prompt("Enter Short Hex.");
-alert(extendHex(shortHex));
+  // Ensure the shortHex has exactly 3 characters
+  if (shortHex.length === 3) {
+    // Extend each color component to two characters
+    shortHex = shortHex.replace(/[0-9a-f]/gi, (match) => match + match);
+  }
+
+  // Add the # prefix and return the full hex code
+  return `#${shortHex.toUpperCase()}`;
+}
+
+// Examples
+console.log(extendHex("#abc"));  // expected output: "#aabbcc"
+console.log(extendHex("abc"));   // expected output: "#aabbcc"
+console.log(extendHex("#AbC"));  // expected output: "#AABBCC"
+console.log(extendHex("#f09"));  // expected output: "#ff0099"
